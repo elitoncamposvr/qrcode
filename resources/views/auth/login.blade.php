ed953cddc6e8ba36159987d9a15b22af89306be0
Eliton Campos <eliton.pecas@gmail.com>
2024-05-29 22:45:04 -0300

    First Commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,34 @@
+<x-guest-layout>
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
+
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+
+        <!-- Email Address -->
+        <div>
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-7">
+            <x-input-label for="password" :value="__('Senha')" />
+
+            <x-text-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="current-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+
+        <div class="flex items-center justify-center mt-7">
+            <x-extra-button>
+                {{ __('entrar') }}
+            </x-extra-button>
+        </div>
+    </form>
+</x-guest-layout>
