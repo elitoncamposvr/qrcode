a1bc853eff7b0a64723000e7ded11901aba43753
Eliton Campos <eliton.pecas@gmail.com>
2024-06-26 23:04:47 -0300

    Route codes.show in codes index fixed 7

--- app/Http/Controllers/CodeController.php
+++ app/Http/Controllers/CodeController.php
@@ -3,106 +3,65 @@
 namespace App\Http\Controllers;
 
 use App\Models\Code;
-use App\Models\CodeClass;
-use App\Models\CodeFamily;
-use App\Models\CodeGroup;
-use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Redirect;
 
 class CodeController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
-        return view('codes', [
-            'codes' => Code::paginate(20),
+        return view('codes',[
+            'codes' => Code::paginate(15),
         ]);
     }
 
+    /**
+     * Show the form for creating a new resource.
+     */
     public function create()
     {
-        return view('codes_create', [
-            'code_class' => CodeClass::all(),
-            'code_family' => CodeFamily::all(),
-            'code_group' => CodeGroup::all(),
-        ]);
+        //
     }
 
+    /**
+     * Store a newly created resource in storage.
+     */
     public function store(Request $request)
     {
-        $request->validate([
-            'class_code' => ['required', 'integer',],
-            'families_code' => ['required', 'integer',],
-            'group_code' => ['required', 'integer',],
-            'description' => ['required', 'string', 'max:255'],
-        ]);
-
-        $code = Code::create([
-            'code' => '0',
-            'designer' => $request->get('designer'),
-            'class_code' => $request->get('class_code'),
-            'families_code' => $request->get('families_code'),
-            'group_code' => $request->get('group_code'),
-            'description' => $request->get('description'),
-        ]);
-//
-        event(new Registered($code));
-
-        $code_all = $request
-                ->get('class_code') . "."
-            . $request->
-            get('families_code') . "."
-            . $request->
-            get('group_code') . "."
-            . $code->id;
-
-        $code_update = Code::find($code->id);
-        $code_update->update([
-            'code' => $code_all,
-        ]);
-
-//        dump($code_all);
-
-        return \redirect(route('codes.index', absolute: false));
-
+        //
     }
 
-    public function edit($id)
+    /**
+     * Display the specified resource.
+     */
+    public function show(Code $code)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Code $code)
     {
         return view('codes_edit');
     }
 
+    /**
+     * Update the specified resource in storage.
+     */
     public function update(Request $request, Code $code)
     {
         //
     }
 
-    public function show($id)
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Code $code)
     {
-//        $codes = DB::table('codes')
-//            ->leftJoin('code_classes', 'codes.class_id', '=', 'code_classes.id')
-//            ->leftJoin('code_families', 'codes.family_id', '=', 'code_families.id')
-//            ->leftJoin('code_groups', 'codes.group_id', '=', 'code_groups.id')
-//            ->where('codes.id', '=', $id)
-//            ->get();
-//
-//        return view('codes_show')->with('code', $codes[0]);
-
-//        dump($codes[0]->code);
-
-        return view('codes_show', [
-            'code' => Code::find($id),
-        ]);
-
-    }
-
-
-    public function destroy(Request $request)
-    {
-        $code = Code::find($request->id);
-        $code->delete();
-
-        return Redirect::to('/codes');
+        //
     }
 }
